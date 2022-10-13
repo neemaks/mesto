e96e132da54374da514a13efeee248734058ff8b
Maksim Mikhailov <maks@neemaks.ru>
2022-10-13 09:31:36 +0700

    added fade in and out to popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,67 @@
-// Находим попап и кнопки
+// *SECTION POPUP Находим попап и кнопки
 const popup = document.querySelector('.popup');
 const openButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-icon');
 
-// Открываем и закрваем попап (заносим данные из профиля) 
+// *SECTION POPUP Открываем и закрваем попап (заносим данные из профиля) 
 const togglePopup = () => {
-  if (!popup.classList.contains('popup_opened')) {
-    nameInput.value = titleName.textContent;
+  if (popup.classList.contains('popup_opened')) {
     jobInput.value = titleJob.textContent;
   }
   popup.classList.toggle('popup_opened');
+  nameInput.value = titleName.textContent;
 }
 
 openButton.addEventListener('click', togglePopup);
-// Добовляем fade out animation на закрытие popup
+
+// *SECTION POPUP Добовляем fade out animation на закрытие popup
 closeButton.addEventListener('click', () => {
   popup.setAttribute("closing", "");
 
   popup.addEventListener('animationend', () => {
-      popup.removeAttribute("closing");
-      togglePopup();
+    popup.removeAttribute("closing");
+    togglePopup();
   }, { once: true });
 });
 
-// Находим форму
+// *SECTION POPUP Находим форму
 const formElement = document.querySelector('.popup__form');
-// Находим поля формы
+// *SECTION POPUP Находим поля формы
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
-// Находим поля для редактироания имени и профессии
+// *SECTION POPUP Находим поля для редактироания имени и профессии
 const titleName = document.querySelector('.profile__title');
 const titleJob = document.querySelector('.profile__subtitle');
 
-// Обработчик «отправки» формы
+// *SECTION POPUP Обработчик «отправки» формы
 function formSubmitHandler(evt) {
-  // Отменяем стандартную отправку формы
+  // *SECTION POPUP Отменяем стандартную отправку формы
   evt.preventDefault();
 
-  // Получаем значение полей jobInput и nameInput из свойства value
+  // *SECTION POPUP Получаем значение полей jobInput и nameInput из свойства value
   const nameValue = nameInput.value;
   const jobeValue = jobInput.value;
 
-  // Выбираем и вставлям новые значения полей
+  // *SECTION POPUP Выбираем и вставлям новые значения полей
   titleName.textContent = nameValue;
   titleJob.textContent = jobeValue;
-  // Дублируем fade out animation на Submit закрытие
-    popup.setAttribute("closing", "");
-    popup.addEventListener('animationend', () => {
-        popup.removeAttribute("closing");
-        togglePopup();
-    }, { once: true });
+  // *SECTION POPUP Дублируем fade out animation на Submit закрытие
+  popup.setAttribute("closing", "");
+  popup.addEventListener('animationend', () => {
+    popup.removeAttribute("closing");
+    togglePopup();
+  }, { once: true });
 }
 
-// Прикрепляем обработчик к форме:
+// *SECTION POPUP Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+// *SECTION ELEMENT Находим лайк
+const likeButton = document.querySelector('.element__like-button');
+// *SECTION ELEMENT Делаем лайк кликабельным
+likeButton.addEventListener('click', () => {
+  likeButton.classList.toggle('element__like-button_active');
+})
+
+likeButton.addEventListener('click', likeButton);
